3d428d0e00647cc6dfb10062fdf0157a23483681
Diego Jimenez <diegojmoravia@gmail.com>
2023-03-05 14:39:01 -0600

    frontend login page added

--- frontend/src/Pages/LogIn.js
+++ frontend/src/Pages/LogIn.js
@@ -1,9 +1,28 @@
 import '../styles/login.css'
-
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 export default function LogIn(){
     return (
+    <Container class="login-container">
+  
+        <form class="sing-in-form">
+            <div class="form-control-cont">
+                <label for="email-address-input">Email address</label>
+                <input id="email-address-input" type="text" />
+            </div>
 
-        <h1>Pagina de login</h1>
+            <div class="form-control-cont">
+                <label for="password-input">Password</label>
+                <input id="password-input" type="password" />
+            </div>
+            <button class="submit-btn" type="submit" variant="success">Acceder</button>
+
+            <Link to="/registration">Registrarse</Link>
+        </form>
+
+
+    </Container>
+   
 
     )
 }
--- frontend/src/Pages/Registration.js
+++ frontend/src/Pages/Registration.js
@@ -1,3 +1,6 @@
+import '../styles/registration.css'
+
+
 export default function Registration(){
     return (
         <h1>Registration</h1>
