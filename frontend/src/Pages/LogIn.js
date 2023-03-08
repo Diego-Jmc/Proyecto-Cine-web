183bc173b5e9139072c59a0ab70ed5f8e8139e93
Diego Jimenez <diegojmoravia@gmail.com>
2023-03-08 00:39:32 -0600

    Register frontend logic finished

--- frontend/src/Pages/LogIn.js
+++ frontend/src/Pages/LogIn.js
@@ -3,19 +3,19 @@ import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 export default function LogIn(){
     return (
-    <Container class="login-container">
+    <Container className="login-container">
   
-        <form class="sing-in-form">
-            <div class="form-control-cont">
-                <label for="email-address-input">Email address</label>
+        <form className="sing-in-form">
+            <div className="form-control-cont">
+                <label htmlFor="email-address-input">Email address</label>
                 <input id="email-address-input" type="text" />
             </div>
 
-            <div class="form-control-cont">
-                <label for="password-input">Password</label>
+            <div className="form-control-cont">
+                <label htmlFor="password-input">Password</label>
                 <input id="password-input" type="password" />
             </div>
-            <button class="submit-btn" type="submit" variant="success">Acceder</button>
+            <button className="submit-btn" type="submit" variant="success">Acceder</button>
 
             <Link to="/registration">Registrarse</Link>
         </form>
--- frontend/src/Pages/Registration.js
+++ frontend/src/Pages/Registration.js
@@ -1,8 +1,99 @@
 import '../styles/registration.css'
+import { Container } from 'react-bootstrap';
+import { useState } from 'react';
+
+const MINUM_DIGITS = 4
+
+export default function Registration() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [validPassword,setValidPassword] = useState(false)
+    const [samePassword,setSamePassword] = useState(false)
+
+  
+    const handleEmailChange = (event) => {
+      setEmail(event.target.value);
+    };
+  
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+
+      isValidPassword(event.target.value) ? setValidPassword(true) : setValidPassword(false)
+
+      confirmPassword === event.target.value && event.target.value !== "" ? setSamePassword(true) : setSamePassword(false)
+    };
+  
+    const handleConfirmPasswordChange = (event) => {
+      setConfirmPassword(event.target.value);
+
+      password === event.target.value && password !== "" ? setSamePassword(true) : setSamePassword(false)
+
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    };
+  
+
+    
+    function isValidPassword(password) {
+
+        if (password.length >= 8) {
+            if (hasMinimun(password,MINUM_DIGITS))
+               
+            return true
+        }
 
 
-export default function Registration(){
+        return false
+
+    }
+
+    function hasMinimun(password,minDigits) {
+
+        const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+        if (password.length > 0) {
+
+            let counter = 0
+
+            for (let i = 0; i < password.length; i++) {
+                if (numbers.some(e => e === password[i]))
+                    counter++
+
+            }
+
+            return counter >= minDigits ? true : false
+        }
+
+        return false
+    }
+
+
     return (
-        <h1>Registration</h1>
+
+
+        <Container>
+  
+          <form onSubmit={handleSubmit}>
+      <div className='register-form-control'>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" className='no-border' value={email} onChange={handleEmailChange} required />
+      </div>
+      <div className='register-form-control' >
+        <label htmlFor="password">Password:</label>
+        <input className = {validPassword? "valid-password" : "no-border"} type="password" id="password" value={password} onChange={handlePasswordChange} required />
+      </div>
+      <div className='register-form-control'>
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input className={samePassword? "valid-password" : "no-border"} type="password" id="confirmPassword" value={confirmPassword} onChange={handleConfirmPasswordChange} required />
+      </div>
+      <button className = "submit-btn" type="submit">Register</button>
+    </form>
+        </Container>
+
+
     )
 }
